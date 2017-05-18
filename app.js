eae40f37eb97d03569821f54cad81bc4a7eb5b3f
mttfshr <matt@mattfisherstudio.com>
2017-05-17 18:22:00 -0700

    Finished tutorial starter files

--- app.js
+++ app.js
@@ -35,14 +35,147 @@ app.get('/',function(req, res){
           });
         });
 
-        res.render('index', {
-          persons: personArr
-        });
+        session
+          .run("MATCH (n:Location) RETURN n")
+          .then(function(result2){
+            var locationArr = [];
+            result2.records.forEach(function(record){
+              locationArr.push(record._fields[0].properties);
+            });
+
+            res.render('index', {
+              persons: personArr,
+              locations: locationArr
+            });
+          })
       })
       .catch(function(error){
         console.log(error);
       });
 });
+
+//Add Person route
+app.post('/person/add', function(req, res){
+  var name = req.body.name;
+  console.log(name);
+
+  session
+    .run("CREATE(n:Person {name:{nameParam}}) RETURN n.name", {nameParam: name})
+    .then(function(result){
+      res.redirect('/');
+      session.close();
+    })
+    .catch(function(error){
+      console.log(error);
+    });
+})
+
+//Add Location route
+app.post('/location/add', function(req, res){
+  var city = req.body.city;
+  var state = req.body.state;
+
+  session
+    .run("CREATE(n:Location {name:{cityParam}, state:{stateParam}}) RETURN n", {cityParam: city, stateParam: state})
+    .then(function(result){
+      res.redirect('/');
+      session.close();
+    })
+    .catch(function(error){
+      console.log(error);
+    });
+})
+
+//Friends connect route
+app.post('/friends/connect', function(req, res){
+  var name1 = req.body.name1;
+  var name2 = req.body.name2;
+  var id = req.body.id;
+
+  session
+    .run("MATCH(a:Person {name:{nameParam1}}), (b:Person{name:{nameParam2}}) MERGE(a)-[r:FRIENDS]->(b) RETURN a,b", {nameParam1: name1, nameParam2: name2})
+    .then(function(result){
+      if(id && id != null){
+        res.redirect('/person/'+id);
+      } else {
+      res.redirect('/');
+    }
+      session.close();
+    })
+    .catch(function(error){
+      console.log(error);
+    });
+});
+
+//Add birthplace route
+app.post('/person/born/add', function(req, res){
+  var name = req.body.name;
+  var city = req.body.city;
+  var state = req.body.state;
+  var year = req.body.year;
+  var id = req.body.id;
+
+  session
+    .run("MATCH(a:Person {name:{nameParam}}), (b:Location{name:{cityParam}, state:{stateParam}}) MERGE(a)-[r:BORN_IN {year:{yearParam}}]->(b) RETURN a,b", {nameParam:name, cityParam:city, stateParam:state, yearParam:year})
+    .then(function(result){
+      if(id && id != null){
+        res.redirect('/person/'+id);
+      } else {
+      res.redirect('/');
+    }
+      session.close();
+    })
+    .catch(function(error){
+      console.log(error);
+    });
+});
+
+// Person route
+app.get('/person/:id', function(req, res){
+  var id = req.params.id;
+
+  session
+    .run("MATCH (a:Person) WHERE id(a)=toInt({idParam}) RETURN a.name as name", {idParam:id})
+    .then(function(result){
+      var name = result.records[0].get("name");
+
+      session
+        .run("OPTIONAL MATCH (a:Person)-[r:BORN_IN]-(b:Location) WHERE id(a)=toInt({idParam}) RETURN b.name as city, b.state as state", {idParam:id})
+        .then(function(result2){
+          var city = result2.records[0].get("city");
+          var state = result2.records[0].get("state");
+
+          session
+              .run("OPTIONAL MATCH (a:Person)-[r:FRIENDS]-(b:Person) WHERE id(a)=toInt({idParam}) RETURN b", {idParam:id})
+              .then(function(result3){
+                  var friendsArr = [];
+
+                  result3.records.forEach(function(record){
+                      if(record._fields[0] != null){
+                          friendsArr.push({
+                              id: record._fields[0].identity.low,
+                              name: record._fields[0].properties.name
+                          });
+                      }
+                  });
+
+                  res.render('person',{
+                      id:id,
+                      name:name,
+                      city:city,
+                      state: state,
+                      friends:friendsArr
+                  });
+
+                  session.close();
+              })
+              .catch(function(error){
+                  console.log(error);
+              });
+        });
+    });
+});
+
 
 app.listen(3000);
 
