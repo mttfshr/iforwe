7368ee57fe44431b5d886cd1e7739b76be7da664
mttfshr <matt@mattfisherstudio.com>
2017-05-22 10:34:31 -0700

    commented out twitter auth

--- app/config/passport.js
+++ app/config/passport.js
@@ -193,87 +193,87 @@ module.exports = function(passport) {
 			}
 		});
 	}));
-
-	// =========================================================================
-	// TWITTER =================================================================
-	// =========================================================================
-	passport.use(new TwitterStrategy({
-		consumerKey     : configAuth.twitterAuth.consumerKey,
-		consumerSecret  : configAuth.twitterAuth.consumerSecret,
-		callbackURL     : configAuth.twitterAuth.callbackURL,
-		passReqToCallback : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
-	},
-	function(req, token, tokenSecret, profile, done) {
-		console.log('twitter req:' + req);
-		console.log('req.user: ' + req.user);
-		console.log('twitter profile.id: ' + profile.id);
-		// asynchronous
-		process.nextTick(function() {
-			// check if the user is already logged in
-			if (!req.user) {
-				User.getBy('user.twitterId', profile.id, function(err, user) {
-					console.log('twitter user: ' + user);
-					if (err)
-						return done(err);
-
-					if (user) {
-						// if there is a user id already but no token (user was linked at one point and then removed)
-						if (!user.twitterToken) {
-							var updateUser = {};
-								updateUser.id = user._id;
-								updateUser.props = {};
-									updateUser.props.twitterToken       = token;
-									updateUser.props.twitterUsername    = profile.username;
-									updateUser.props.twitterDisplayName = profile.displayName;
-							User.update(updateUser, function(err, user) {
-								if (err)
-									throw err;
-								return done(null, user);
-							});
-						}
-
-						return done(null, user); // user found, return that user
-					} else {
-						// if there is no user, create them
-						var newUser = {};
-							newUser.twitter.id          = profile.id;
-							newUser.twitter.token       = token;
-							newUser.twitter.username    = profile.username;
-							newUser.twitter.displayName = profile.displayName;
-
-						User.create(newUser, function (err, user) {
-							if (err)
-								return next(err);
-							return done(null, user);
-						});
-					}
-				});
-			} else {
-				// user already exists and is logged in, we have to link accounts
-				// but check if tht twitter is already linked
-				User.getBy('user.twitterId', profile.id, function(err, user) {
-					if (err)
-						return done(err);
-
-					if (user) {
-						return done(null, false, req.flash('connectMessage', 'That twitter account is already linked!'));
-					} else {
-						var updateUser = {};
-							updateUser.id = req.user._id;
-							updateUser.props = {};
-								updateUser.props.twitterId          = profile.id;
-								updateUser.props.twitterToken       = token;
-								updateUser.props.twitterUsername    = profile.username;
-								updateUser.props.twitterDisplayName = profile.displayName;
-
-						User.update(updateUser, function(err, user) {
-							if (err)
-								throw err;
-							return done(null, user);
-						});
-					}
-				});
-			}
-		});
-	}));
+	//
+	// // =========================================================================
+	// // TWITTER =================================================================
+	// // =========================================================================
+	// passport.use(new TwitterStrategy({
+	// 	consumerKey     : configAuth.twitterAuth.consumerKey,
+	// 	consumerSecret  : configAuth.twitterAuth.consumerSecret,
+	// 	callbackURL     : configAuth.twitterAuth.callbackURL,
+	// 	passReqToCallback : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
+	// },
+	// function(req, token, tokenSecret, profile, done) {
+	// 	console.log('twitter req:' + req);
+	// 	console.log('req.user: ' + req.user);
+	// 	console.log('twitter profile.id: ' + profile.id);
+	// 	// asynchronous
+	// 	process.nextTick(function() {
+	// 		// check if the user is already logged in
+	// 		if (!req.user) {
+	// 			User.getBy('user.twitterId', profile.id, function(err, user) {
+	// 				console.log('twitter user: ' + user);
+	// 				if (err)
+	// 					return done(err);
+	//
+	// 				if (user) {
+	// 					// if there is a user id already but no token (user was linked at one point and then removed)
+	// 					if (!user.twitterToken) {
+	// 						var updateUser = {};
+	// 							updateUser.id = user._id;
+	// 							updateUser.props = {};
+	// 								updateUser.props.twitterToken       = token;
+	// 								updateUser.props.twitterUsername    = profile.username;
+	// 								updateUser.props.twitterDisplayName = profile.displayName;
+	// 						User.update(updateUser, function(err, user) {
+	// 							if (err)
+	// 								throw err;
+	// 							return done(null, user);
+	// 						});
+	// 					}
+	//
+	// 					return done(null, user); // user found, return that user
+	// 				} else {
+	// 					// if there is no user, create them
+	// 					var newUser = {};
+	// 						newUser.twitter.id          = profile.id;
+	// 						newUser.twitter.token       = token;
+	// 						newUser.twitter.username    = profile.username;
+	// 						newUser.twitter.displayName = profile.displayName;
+	//
+	// 					User.create(newUser, function (err, user) {
+	// 						if (err)
+	// 							return next(err);
+	// 						return done(null, user);
+	// 					});
+	// 				}
+	// 			});
+	// 		} else {
+	// 			// user already exists and is logged in, we have to link accounts
+	// 			// but check if tht twitter is already linked
+	// 			User.getBy('user.twitterId', profile.id, function(err, user) {
+	// 				if (err)
+	// 					return done(err);
+	//
+	// 				if (user) {
+	// 					return done(null, false, req.flash('connectMessage', 'That twitter account is already linked!'));
+	// 				} else {
+	// 					var updateUser = {};
+	// 						updateUser.id = req.user._id;
+	// 						updateUser.props = {};
+	// 							updateUser.props.twitterId          = profile.id;
+	// 							updateUser.props.twitterToken       = token;
+	// 							updateUser.props.twitterUsername    = profile.username;
+	// 							updateUser.props.twitterDisplayName = profile.displayName;
+	//
+	// 					User.update(updateUser, function(err, user) {
+	// 						if (err)
+	// 							throw err;
+	// 						return done(null, user);
+	// 					});
+	// 				}
+	// 			});
+	// 		}
+	// 	});
+	// }));
 };
